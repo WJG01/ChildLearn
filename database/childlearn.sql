78b944e40ec554352d242f6f593156090093acdf
WJ <anonymousname1904@gmail.com>
2023-07-10 13:59:52 +0800

    UpdatedLatestSQLDatabase

--- database/childlearn.sql
+++ database/childlearn.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 30, 2023 at 04:50 PM
+-- Generation Time: Jul 10, 2023 at 05:57 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -133,15 +133,15 @@ CREATE TABLE IF NOT EXISTS `course` (
 --
 
 INSERT INTO `course` (`course_id`, `course_title`, `course_category`, `course_cover`, `course_description`, `course_create_date`, `teac_id`) VALUES
-(1, 'Phonics Fun', 'Language and Literacy', 'Phonics Fun.jpeg', 'This course introduces children to phonics through interactive games, songs, and activities.', '2023-06-30', 1),
-(2, 'Storytelling Adventures', 'Language and Literacy', 'Storytelling Adventures.jpeg', 'In this course, children embark on exciting storytelling adventures. They learn how to create characters, build narratives, and express their ideas through storytelling techniques, encouraging creativity and language development.', '2023-06-30', 1),
-(3, 'Word Wizards', 'Language and Literacy', 'Word Wizards.jpeg', 'Word Wizards is a vocabulary-building course that helps children expand their word bank, improve word usage, and enhance their language skills through engaging word games, puzzles, and word association activities.', '2023-06-30', 1),
-(4, 'Number Ninjas', 'Mathematics and Logic', 'Number Ninjas.jpeg', 'Number Ninjas is a course designed to develop children\'s number sense and counting skills. Through interactive exercises, games, and counting adventures, children learn to recognize numbers, count objects, and understand basic mathematical concepts.', '2023-06-30', 2),
-(5, 'Shape Explorers', 'Mathematics and Logic', 'Shape Explorers.jpeg', 'Shape Explorers takes children on a journey to discover different shapes and patterns. Through hands-on activities, shape recognition games, and pattern-building challenges, children develop spatial awareness and logical thinking skills.', '2023-06-30', 2),
-(6, 'Problem Solvers Club', 'Mathematics and Logic', 'Problem Solvers Club.jpeg', 'In the Problem Solvers Club, children are introduced to critical thinking and problem-solving skills. Through puzzles, riddles, and interactive challenges, they learn to analyze situations, make decisions, and find solutions using their mathematical abili', '2023-06-30', 2),
-(7, 'Nature Detectives', 'Science and Discovery', 'Nature Detectives.jpeg', 'Nature Detectives is an exploration course that helps children discover the wonders of nature. Through interactive lessons, virtual field trips, and nature-themed activities, children learn about plants, animals, ecosystems, and develop an appreciation fo', '2023-06-30', 3),
-(8, 'Science Lab Explorers', 'Science and Discovery', 'Science Lab Explorers.jpeg', 'Science Lab Explorers takes children on a virtual journey to the science lab. Through hands-on experiments, demonstrations, and scientific investigations, children explore basic scientific concepts, such as states of matter, electricity, or simple chemica', '2023-06-30', 3),
-(9, 'STEM Builders', 'Science and Discovery', 'STEM Builders.jpeg', 'STEM Builders is a course that introduces children to the exciting world of STEM. Through engaging projects, experiments, and interactive challenges, children develop critical thinking, problem-solving, and engineering skills while exploring topics like r', '2023-06-30', 3);
+(1, 'Phonics Fun', 'Language and Literacy', 'phonicfun_course_coverpage.jpeg', 'This course introduces children to phonics through interactive games, songs, and activities.', '2023-06-30', 1),
+(2, 'Storytelling Adventures', 'Language and Literacy', 'storytelling_course_coverpage.jpeg', 'In this course, children embark on exciting storytelling adventures. They learn how to create characters, build narratives, and express their ideas through storytelling techniques, encouraging creativity and language development.', '2023-06-30', 1),
+(3, 'Word Wizards', 'Language and Literacy', 'wordwizard_course_coverpage.jpeg', 'Word Wizards is a vocabulary-building course that helps children expand their word bank, improve word usage, and enhance their language skills through engaging word games, puzzles, and word association activities.', '2023-06-30', 1),
+(4, 'Number Ninjas', 'Mathematics and Logic', 'numberninjas_course_coverpage', 'Number Ninjas is a course designed to develop children\'s number sense and counting skills. Through interactive exercises, games, and counting adventures, children learn to recognize numbers, count objects, and understand basic mathematical concepts.', '2023-06-30', 2),
+(5, 'Shape Explorers', 'Mathematics and Logic', 'shapeexplorer_course_coverpage.jpeg', 'Shape Explorers takes children on a journey to discover different shapes and patterns. Through hands-on activities, shape recognition games, and pattern-building challenges, children develop spatial awareness and logical thinking skills.', '2023-06-30', 2),
+(6, 'Problem Solvers Club', 'Mathematics and Logic', 'problemsolverclub_course_coverpage.jpeg', 'In the Problem Solvers Club, children are introduced to critical thinking and problem-solving skills. Through puzzles, riddles, and interactive challenges, they learn to analyze situations, make decisions, and find solutions using their mathematical abili', '2023-06-30', 2),
+(7, 'Nature Detectives', 'Science and Discovery', 'naturedetectives_course_coverpage.jpeg', 'Nature Detectives is an exploration course that helps children discover the wonders of nature. Through interactive lessons, virtual field trips, and nature-themed activities, children learn about plants, animals, ecosystems, and develop an appreciation fo', '2023-06-30', 3),
+(8, 'Science Lab Explorers', 'Science and Discovery', 'sciencelabexplorer_course_coverpage.jpeg', 'Science Lab Explorers takes children on a virtual journey to the science lab. Through hands-on experiments, demonstrations, and scientific investigations, children explore basic scientific concepts, such as states of matter, electricity, or simple chemica', '2023-06-30', 3),
+(9, 'STEM Builders', 'Science and Discovery', 'stembuilder_course_coverpage.jpeg', 'STEM Builders is a course that introduces children to the exciting world of STEM. Through engaging projects, experiments, and interactive challenges, children develop critical thinking, problem-solving, and engineering skills while exploring topics like r', '2023-06-30', 3);
 
 -- --------------------------------------------------------
 
@@ -153,6 +153,7 @@ DROP TABLE IF EXISTS `course_chapter`;
 CREATE TABLE IF NOT EXISTS `course_chapter` (
   `chapter_id` int NOT NULL AUTO_INCREMENT,
   `chapter_title` varchar(40) NOT NULL,
+  `chapter_order` int NOT NULL,
   `content_text` longtext NOT NULL,
   `content_image` longtext NOT NULL,
   `content_video` longtext,
@@ -164,34 +165,34 @@ CREATE TABLE IF NOT EXISTS `course_chapter` (
 -- Dumping data for table `course_chapter`
 --
 
-INSERT INTO `course_chapter` (`chapter_id`, `chapter_title`, `content_text`, `content_image`, `content_video`, `course_id`) VALUES
-(1, 'Letter Magic', 'Chapter 1 teaching content paragraph 1...', 'Letter Magic.jpeg', 'Letter Magic.mp4', 1),
-(2, 'Sound Detectives', 'Chapter 2 teaching content paragraph 1...', 'Sound Detectives.jpeg', 'Sound Detectives.mp4', 1),
-(3, 'Reading Adventures', 'Chapter 3 teaching content paragraph 1...', 'Reading Adventures.jpeg', 'Reading Adventures.mp4', 1),
-(4, 'Creating Characters', 'Chapter 1 teaching content paragraph 1...', 'Creating Characters.jpeg', 'Creating Characters.mp4', 2),
-(5, 'Building Plotlines', 'Chapter 2 teaching content paragraph 1...', 'Building Plotlines.jpeg', 'Building Plotlines.mp4', 2),
-(6, 'Expressive Storytelling', 'Chapter 3 teaching content paragraph 1...', 'Expressive Storytelling.jpeg', 'Expressive Storytelling.mp4', 2),
-(7, 'Vocabulary Voyage', 'Chapter 1 teaching content paragraph 1...', 'Vocabulary Voyage.jpeg', 'Vocabulary Voyage.mp4', 3),
-(8, 'Word Puzzles Galore', 'Chapter 2 teaching content paragraph 1...', 'Word Puzzles Galore.jpeg', 'Word Puzzles Galore.mp4', 3),
-(9, 'Creative Word Play', 'Chapter 3 teaching content paragraph 1...', 'Creative Word Play.jpeg', 'Creative Word Play.mp4', 3),
-(10, 'Counting Conquerors', 'Chapter 1 teaching content paragraph 1...', 'Counting Conquerors.jpeg', 'Counting Conquerors.mp4', 4),
-(11, 'Addition Adventures', 'Chapter 2 teaching content paragraph 1...', 'Addition Adventures.jpeg', 'Addition Adventures.mp4', 4),
-(12, 'Subtraction Superstars', 'Chapter 3 teaching content paragraph 1...', 'Subtraction Superstars.jpeg', 'Subtraction Superstars.mp4', 4),
-(13, 'Exploring 2D Shapes', 'Chapter 1 teaching content paragraph 1...', 'Exploring 2D Shapes.jpeg', 'Exploring 2D Shapes.mp4', 5),
-(14, 'Discovering 3D Shapes', 'Chapter 2 teaching content paragraph 1...', 'Discovering 3D Shapes.jpeg', 'Discovering 3D Shapes.mp4', 5),
-(15, 'Pattern Puzzlers', 'Chapter 3 teaching content paragraph 1...', 'Pattern Puzzlers.jpeg', 'Pattern Puzzlers.mp4', 5),
-(16, 'Logic Labyrinth', 'Chapter 1 teaching content paragraph 1...', 'Logic Labyrinth.jpeg', 'Logic Labyrinth.mp4', 6),
-(17, 'Critical Thinking Challenges', 'Chapter 2 teaching content paragraph 1...', 'Critical Thinking Challenges.jpeg', 'Critical Thinking Challenges.mp4', 6),
-(18, 'Puzzle Solvers Society', 'Chapter 3 teaching content paragraph 1...', 'Puzzle Solvers Society.jpeg', 'Puzzle Solvers Society.mp4', 6),
-(19, 'Plants and Their World', 'Chapter 1 teaching content paragraph 1...', 'Plants and Their World.jpeg', 'Plants and Their World.mp4', 7),
-(20, 'Animal Kingdom Explorations', 'Chapter 2 teaching content paragraph 1...', 'Animal Kingdom Explorations.jpeg', 'Animal Kingdom Explorations.mp4', 7),
-(21, 'Ecosystem Expeditions', 'Chapter 3 teaching content paragraph 1...', 'Ecosystem Expeditions.jpeg', 'Ecosystem Expeditions.mp4', 7),
-(22, 'States of Matter Exploration', 'Chapter 1 teaching content paragraph 1...', 'States of Matter Exploration.jpeg', 'States of Matter Exploration.mp4', 8),
-(23, 'Electricity and Circuitry Adventures', 'Chapter 2 teaching content paragraph 1...', 'Electricity and Circuitry Adventures.jpeg', 'Electricity and Circuitry Adventures.mp4', 8),
-(24, 'Chemical Reactions Unveiled', 'Chapter 3 teaching content paragraph 1...', 'Chemical Reactions Unveiled.jpeg', 'Chemical Reactions Unveiled.mp4', 8),
-(25, 'Introduction to Robotics', 'Chapter 1 teaching content paragraph 1...', 'Introduction to Robotics.jpeg', 'Introduction to Robotics.mp4', 9),
-(26, 'Building Simple Machines', 'Chapter 2 teaching content paragraph 1...', 'Building Simple Machines.jpeg', 'Building Simple Machines.mp4', 9),
-(27, 'Coding Adventures', 'Chapter 3 teaching content paragraph 1...', 'Coding Adventures.jpeg', 'Coding Adventures.mp4', 9);
+INSERT INTO `course_chapter` (`chapter_id`, `chapter_title`, `chapter_order`, `content_text`, `content_image`, `content_video`, `course_id`) VALUES
+(1, 'Letter Magic', 1, 'Welcome to the exciting world of Letter Magic! Get ready to embark on a thrilling journey through the alphabet, from A to Z. Each letter holds a special power and opens up a whole new world of words and wonders. Let\'s explore the magic of each letter and learn more about them:\n\nA - A is for Apple: A juicy and delicious fruit that starts with the letter A. Can you think of any other words that start with the letter A?\n\nB - B is for Ball: A round object that we can play with. What other words can you think of that begin with the letter B?\n\nC - C is for Cat: A furry and playful animal that starts with the letter C. Can you name any other animals that begin with the letter C?\n\nD - D is for Dog: A loyal and friendly animal that starts with the letter D. Can you think of any other words that start with the letter D?\n\nE - E is for Elephant: A magnificent and gentle giant that begins with the letter E. What other words can you think of that start with the letter E?\n\nF - F is for Flower: Beautiful and colorful plants that begin with the letter F. Can you name any other words that start with the letter F?\n\nG - G is for Giraffe: A tall and graceful animal that starts with the letter G. What other words can you think of that begin with the letter G?\n\nH - H is for Hat: An accessory that we wear on our heads. Can you think of any other words that start with the letter H?\n\nI - I is for Ice Cream: A sweet and cold treat that starts with the letter I. Can you name any other words that begin with the letter I?\n\nJ - J is for Jellyfish: An interesting sea creature that starts with the letter J. What other words can you think of that start with the letter J?\n\nK - K is for Key: An object we use to unlock doors. Can you think of any other words that start with the letter K?\n\nL - L is for Lion: A majestic and powerful animal that starts with the letter L. Can you name any other words that begin with the letter L?\n\nM - M is for Monkey: A playful and curious creature that starts with the letter M. What other words can you think of that start with the letter M?\n\nN - N is for Nest: A cozy home built by birds. Can you think of any other words that start with the letter N?\n\nO - O is for Octopus: A fascinating sea creature with many tentacles. Can you name any other words that begin with the letter O?\n\nP - P is for Penguin: A cute and waddling bird that starts with the letter P. What other words can you think of that start with the letter P?\n\nQ - Q is for Queen: A royal title for a female ruler. Can you think of any other words that start with the letter Q?\n\nR - R is for Rainbow: A beautiful arc of colors that appears after the rain. Can you name any other words that begin with the letter R?\n\nS - S is for Sun: The bright and shining star that gives us light and warmth. What other words can you think of that start with the letter S?\n\nT - T is for Tree: A tall and sturdy plant with branches and leaves. Can you think of any other words that start with the letter T?\n\nU - U is for Unicorn: A mythical and magical creature with a horn on its head. Can you name any other words that begin with the letter U?\n\nV - V is for Vegetable: Healthy and nutritious plants that we eat. What other words can you think of that start with the letter V?\n\nW - W is for Whale: A massive and gentle creature that lives in the ocean. Can you think of any other words that start with the letter W?\n\nX - X is for Xylophone: A musical instrument with wooden keys. Can you name any other words that begin with the letter X?\n\nY - Y is for Yellow: A bright and cheerful color that starts with the letter Y. What other words can you think of that start with the letter Y?\n\nZ - Z is for Zebra: A striped and unique animal that begins with the letter Z. Can you think of any other words that start with the letter Z?\n\nLet\'s continue our journey through the alphabet and explore the magic of each letter. Together, we\'ll learn to recognize, write, and use the letters to create words. Get ready to unlock the power of the ABCs and embark on an extraordinary learning adventure with Letter Magic!', 'lettermagic_chapter_contentimage.jpg', 'lettermagic_chapter_contentvideo.mp4', 1),
+(2, 'Sound Detectives', 2, 'Chapter 2 teaching content paragraph 1...', 'Sound Detectives.jpeg', 'Sound Detectives.mp4', 1),
+(3, 'Reading Adventures', 3, 'Chapter 3 teaching content paragraph 1...', 'Reading Adventures.jpeg', 'Reading Adventures.mp4', 1),
+(4, 'Creating Characters', 1, 'Chapter 1 teaching content paragraph 1...', 'Creating Characters.jpeg', 'Creating Characters.mp4', 2),
+(5, 'Building Plotlines', 2, 'Chapter 2 teaching content paragraph 1...', 'Building Plotlines.jpeg', 'Building Plotlines.mp4', 2),
+(6, 'Expressive Storytelling', 3, 'Chapter 3 teaching content paragraph 1...', 'Expressive Storytelling.jpeg', 'Expressive Storytelling.mp4', 2),
+(7, 'Vocabulary Voyage', 1, 'Chapter 1 teaching content paragraph 1...', 'Vocabulary Voyage.jpeg', 'Vocabulary Voyage.mp4', 3),
+(8, 'Word Puzzles Galore', 2, 'Chapter 2 teaching content paragraph 1...', 'Word Puzzles Galore.jpeg', 'Word Puzzles Galore.mp4', 3),
+(9, 'Creative Word Play', 3, 'Chapter 3 teaching content paragraph 1...', 'Creative Word Play.jpeg', 'Creative Word Play.mp4', 3),
+(10, 'Counting Conquerors', 1, 'Chapter 1 teaching content paragraph 1...', 'Counting Conquerors.jpeg', 'Counting Conquerors.mp4', 4),
+(11, 'Addition Adventures', 2, 'Chapter 2 teaching content paragraph 1...', 'Addition Adventures.jpeg', 'Addition Adventures.mp4', 4),
+(12, 'Subtraction Superstars', 3, 'Chapter 3 teaching content paragraph 1...', 'Subtraction Superstars.jpeg', 'Subtraction Superstars.mp4', 4),
+(13, 'Exploring 2D Shapes', 1, 'Chapter 1 teaching content paragraph 1...', 'Exploring 2D Shapes.jpeg', 'Exploring 2D Shapes.mp4', 5),
+(14, 'Discovering 3D Shapes', 2, 'Chapter 2 teaching content paragraph 1...', 'Discovering 3D Shapes.jpeg', 'Discovering 3D Shapes.mp4', 5),
+(15, 'Pattern Puzzlers', 3, 'Chapter 3 teaching content paragraph 1...', 'Pattern Puzzlers.jpeg', 'Pattern Puzzlers.mp4', 5),
+(16, 'Logic Labyrinth', 1, 'Chapter 1 teaching content paragraph 1...', 'Logic Labyrinth.jpeg', 'Logic Labyrinth.mp4', 6),
+(17, 'Critical Thinking Challenges', 2, 'Chapter 2 teaching content paragraph 1...', 'Critical Thinking Challenges.jpeg', 'Critical Thinking Challenges.mp4', 6),
+(18, 'Puzzle Solvers Society', 3, 'Chapter 3 teaching content paragraph 1...', 'Puzzle Solvers Society.jpeg', 'Puzzle Solvers Society.mp4', 6),
+(19, 'Plants and Their World', 1, 'Chapter 1 teaching content paragraph 1...', 'Plants and Their World.jpeg', 'Plants and Their World.mp4', 7),
+(20, 'Animal Kingdom Explorations', 2, 'Chapter 2 teaching content paragraph 1...', 'Animal Kingdom Explorations.jpeg', 'Animal Kingdom Explorations.mp4', 7),
+(21, 'Ecosystem Expeditions', 3, 'Chapter 3 teaching content paragraph 1...', 'Ecosystem Expeditions.jpeg', 'Ecosystem Expeditions.mp4', 7),
+(22, 'States of Matter Exploration', 1, 'Chapter 1 teaching content paragraph 1...', 'States of Matter Exploration.jpeg', 'States of Matter Exploration.mp4', 8),
+(23, 'Electricity and Circuitry Adventures', 2, 'Chapter 2 teaching content paragraph 1...', 'Electricity and Circuitry Adventures.jpeg', 'Electricity and Circuitry Adventures.mp4', 8),
+(24, 'Chemical Reactions Unveiled', 3, 'Chapter 3 teaching content paragraph 1...', 'Chemical Reactions Unveiled.jpeg', 'Chemical Reactions Unveiled.mp4', 8),
+(25, 'Introduction to Robotics', 1, 'Chapter 1 teaching content paragraph 1...', 'Introduction to Robotics.jpeg', 'Introduction to Robotics.mp4', 9),
+(26, 'Building Simple Machines', 2, 'Chapter 2 teaching content paragraph 1...', 'Building Simple Machines.jpeg', 'Building Simple Machines.mp4', 9),
+(27, 'Coding Adventures', 3, 'Chapter 3 teaching content paragraph 1...', 'Coding Adventures.jpeg', 'Coding Adventures.mp4', 9);
 
 -- --------------------------------------------------------
 
